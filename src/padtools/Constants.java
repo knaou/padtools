d00d22dc8ec11319d9acbbacaff40779a1d49cc0
naou <monaou@gmail.com>
2016-01-01 17:53:47 +0900

    タイポ修正

--- src/padtools/Constants.java
+++ src/padtools/Constants.java
@@ -4,6 +4,6 @@ package padtools;
  * アプリケーションの情報を保持するクラス
  */
 public class Constants {
-    public final static String APP_NAME = "PadYTools";
+    public final static String APP_NAME = "PadTools";
     public final static String APP_VERSION = "1.1";
 }
